4bc9f15e72be3ae81000087b7f226f40b0714329
Joe Corall <jjc223@lehigh.edu>
2025-01-10 17:53:04 -0500

    add more outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,12 @@ output "gsa" {
   value       = google_service_account.gsa.email
   description = "The GSA the Vault instance runs as."
 }
+
+output "key_bucket" {
+  value = google_storage_bucket.vault["key"].name
+}
+
+
+output "repo" {
+  value = google_artifact_registry_repository.private
+}
